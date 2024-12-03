ca004a63bc6bdd8819b7b4d7ec106f2982787df8
Thomas Stokes <git@tombl.dev>
2024-12-03 18:17:27 +0800

    write file completely before piping to command

--- cache.go
+++ cache.go
@@ -9,12 +9,13 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 var _ http.RoundTripper = &Client{}
 
 func (c *Client) getCachePath(url *url.URL) string {
-	name := strings.ReplaceAll(url.Host+"/"+url.Path, "/", "_")
+	name := strings.ReplaceAll(url.Host+url.Path, "/", "_")
 	if name == "" {
 		name = "index.html"
 	}
@@ -29,52 +30,58 @@ func (c *Client) invalidate(req *http.Request) error {
 	return nil
 }
 
-type cachingBody struct {
-	io.Reader
-	httpBody io.Closer
-	file     *os.File
-}
-
-func (c *cachingBody) Close() error {
-	err := errors.Join(c.httpBody.Close(), c.file.Close())
-	if err != nil {
-		return err
-	}
-
-	return os.Rename(c.file.Name(), strings.TrimSuffix(c.file.Name(), ".dl"))
-}
-
 func (c *Client) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.AddCookie(&http.Cookie{Name: "session", Value: c.sessionCookie})
 	req.Header.Set("User-Agent", "github.com/tombl/aoc")
 
+	if !(req.Method == http.MethodGet || req.Method == "") {
+		return http.DefaultTransport.RoundTrip(req)
+	}
+
 	path := c.getCachePath(req.URL)
-	if req.Method == http.MethodGet || req.Method == "" {
-		if file, err := os.Open(path); err == nil {
-			return &http.Response{
-				StatusCode: http.StatusOK,
-				Body:       file,
-			}, nil
+	stat, err := os.Stat(path)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return nil, fmt.Errorf("checking cache file: %w", err)
+	}
+
+	now := time.Now().UTC()
+	latestRelease := now.Truncate(24 * time.Hour).Add(5 * time.Hour)
+	if now.Before(latestRelease) {
+		latestRelease = latestRelease.Add(-24 * time.Hour)
+	}
+
+	if err != nil || stat.ModTime().Before(latestRelease) {
+		resp, err := http.DefaultTransport.RoundTrip(req)
+		if err != nil {
+			return resp, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return resp, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		}
+
+		tempPath := path + ".dl"
+
+		file, err := os.Create(tempPath)
+		if err != nil {
+			return resp, fmt.Errorf("creating cache file: %w", err)
+		}
+		defer file.Close()
+
+		if _, err := io.Copy(file, resp.Body); err != nil {
+			return resp, fmt.Errorf("writing cache file: %w", err)
+		}
+
+		if err := os.Rename(tempPath, path); err != nil {
+			return resp, fmt.Errorf("renaming cache file: %w", err)
 		}
 	}
 
-	resp, err := http.DefaultTransport.RoundTrip(req)
+	file, err := os.Open(path)
 	if err != nil {
-		return resp, err
+		return nil, fmt.Errorf("opening cache file: %w", err)
 	}
 
-	if resp.StatusCode != http.StatusOK {
-		return resp, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-	}
-
-	file, err := os.Create(path + ".dl")
-	if err != nil {
-		resp.Body.Close()
-		return resp, fmt.Errorf("creating cache file: %w", err)
-	}
-	resp.Body = &cachingBody{
-		Reader:   io.TeeReader(resp.Body, file),
-		httpBody: resp.Body, file: file,
-	}
-	return resp, nil
+	return &http.Response{StatusCode: http.StatusOK, Body: file}, nil
 }
